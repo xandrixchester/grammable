8543eb178ae4d9d956a181b21270c5625564ef5d
xandrix <xandrixecklor@hotmaill.com>
2019-06-09 18:38:11 +0000

    Image Validation error No error previously

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -172,8 +172,14 @@ RSpec.describe GramsController, type: :controller do
       sign_in user
 
 
-			post :create, params: { gram: { message: 'Hello!' } }
-			expect(response).to redirect_to root_path
+			post :create, params: {
+			    gram: {
+			      message: 'Hello!',
+			      picture: fixture_file_upload("/picture.png", 'image/png')
+			    }
+			  }		
+			  	
+  		expect(response).to redirect_to root_path
 
 			gram = Gram.last
 			expect(gram.message).to eq("Hello!")
